3aa34028f7dc690b8bb80a27bd0e34a1ff6cf945
Асадбек* <asadbekme2002@gmail.com>
2023-02-15 15:48:31 +0500

    Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: 'hsl(209, 23%, 22%)',
+        darkModeBg: 'hsl(207, 26%, 17%)',
+        veryDarkBlue: 'hsl(200, 15%, 8%)',
+        darkGray: 'hsl(0, 0%, 52%)',
+        lightModeBg: 'hsl(0, 0%, 98%)',
+        white: 'hsl(0, 0%, 100%)',
+        black: 'hsl(0, 0%, 0%)'
+      },
+      fontFamily: {
+        nunitoSans: ["Nunito Sans", 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif']
+      }
+    },
+    screens: {
+      xs: '375px',
+      sm: '576px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      xxl: '1536px'
+    }
   },
   plugins: [],
 }
